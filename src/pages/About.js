db26c7323798847b59953d60a53dae2f05fbe176
Erwin Corzo <desarrollo305gmail.com>
2022-07-03 12:11:28 -0500

    finish about me

--- src/pages/About.js
+++ src/pages/About.js
@@ -14,30 +14,51 @@ export default function About(){
                 </div>
             </div>
             
-        <Sidebar />
+            <Sidebar />
 
         <div id="general-info-content-about">
             
             <div id="personal-title">
-                <h1>ABOUT ME. <span>Software Developer</span></h1>
+                <div id="title-content">
+                    <h2>Know <span className='underrescord'>Me</span> More</h2>
+                </div>
             </div> 
 
-            <div id="description-about">
-                personal description
-            </div>
-            <div id="description-table">
-                table description
-            </div>
-            <div id="details">
-                Details footer
+                <div id="description-about">
+
+                    <div id="about-content">
+                        <h3> I'm <span className='text-principal-color'>Erwin Corzo,</span> a software developer</h3>
+                        <p>this is a text for test, here all is false. this is a text for test, here all is false. this is a text for test, here all is false. this is a text for test, here all is false. this is a text for test, here all is false.</p>
+
+                        <p>this is a text for test, here all is false. this is a text for test, here all is false. this is a text for test, here all is false. this is a text for test, here all is false. this is a text for test, here all is false.</p>
+                    </div>
+                    
+                    <div id="description-table">
+                        <ul>
+                            <li><span className="fw-table">Name: </span>Erwin Corzo</li>
+                            <li><span className="fw-table">Email: </span><span className="text-principal-color">erwinch18@gmail.com</span></li>
+                            <li><span className="fw-table">Age: </span>33</li>
+                            <li><span className="fw-table">From:</span>Bogotá, Colombia</li>
+                        </ul>
+                        <button className="btn-dow">Download CV</button>
+                    </div>
+                </div>
+                <div id="details">
+                        <div>
+                            <p className="yearx">5+</p>
+                            <p className="yeard">Years Experiance</p>
+                        </div>
+                        <div>
+                            <p className="yearx">3+</p>
+                            <p className="yeard">Lenguaje Programing</p>
+                        </div>
+                        <div>
+                            <p className="yearx">3+</p>
+                            <p className="yeard">Personal Project</p>
+                        </div>
+                </div>
             </div>
         </div>
-
-
-        </div>
-
-
-
         {/**/}
     </>);
 }
